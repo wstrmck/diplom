8ae46f9b0d6fb8f4800b8adb09a78cb210bb2397
Nikita Gusev <inojeen22505@yandex.ru>
2025-05-16 17:47:14 +0300

    последние правки по вёрскте

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
 window.addEventListener('scroll', function () {
     document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 135);
 })
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('#top').fadeIn();
+        } else {
+            $('#top').fadeOut();
+        }
+    });
+
+    $('#top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 300);
+        return false;
+    });
+});
